d84a685404739505a20f6e05881def80e197031e
Aug Menciano <augustus.menciano@cloudemployee.com.au>
2021-07-20 01:03:05 +0800

    creature-study-convertions-1

--- src/project.eth.sol
+++ src/project.eth.sol
@@ -1,20 +1,228 @@
 pragma solidity ^0.7.4;
 
 contract Cryptomons {
-    
+    // Cryptomons aka creatures, familiars, etc
     // 151 different Cryptomon species implemented and saved in the following enum variable.
-    enum Species { BULBASAUR, IVYSAUR, VENUSAUR, CHARMANDER, CHARMELEON, CHARIZARD, SQUIRTLE, WARTORTLE, BLASTOISE, CATERPIE,
-    METAPOD, BUTTERFREE, WEEDLE, KAKUNA, BEEDRILL, PIDGEY, PIDGEOTTO, PIDGEOT, RATTATA, RATICATE, SPEAROW, FEAROW, EKANS,
-    ARBOK, PIKACHU, RAICHU, SANDSHREW, SANDSLASH, NIDORAN_F, NIDORINA, NIDOQUEEN, NIDORAN_M, NIDORINO, NIDOKING, CLEFAIRY,
-    CLEFABLE, VULPIX, NINETALES, JIGGLYPUFF, WIGGLYTUFF, ZUBAT, GOLBAT, ODDISH, GLOOM, VILEPLUME, PARAS, PARASECT, VENONAT,
-    VENOMOTH, DIGLETT, DUGTRIO, MEOWTH, PERSIAN, PSYDUCK, GOLDUCK, MANKEY, PRIMEAPE, GROWLITHE, ARCANINE, POLIWAG, POLIWHIRL,
-    POLIWRATH, ABRA, KADABRA, ALAKAZAM, MACHOP, MACHOKE, MACHAMP, BELLSPROUT, WEEPINBELL, VICTREEBEL, TENTACOOL, TENTACRUEL,
-    GEODUDE, GRAVELER, GOLEM, PONYTA, RAPIDASH, SLOWPOKE, SLOWBRO, MAGNEMITE, MAGNETON, FARFETCH_D, DODUO, DODRIO, SEEL,
-    DEWGONG, GRIMER, MUK, SHELLDER, CLOYSTER, GASTLY, HAUNTER, GENGAR, ONIX, DROWZEE, HYPNO, KRABBY, KINGLER, VOLTORB,
-    ELECTRODE, EXEGGCUTE, EXEGGUTOR, CUBONE, MAROWAK, HITMONLEE, HITMONCHAN, LICKITUNG, KOFFING, WEEZING, RHYHORN, RHYDON,
-    CHANSEY, TANGELA, KANGASKHAN, HORSEA, SEADRA, GOLDEEN, SEAKING, STARYU, STARMIE, MR_MIME, SCYTHER, JYNX, ELECTABUZZ,
-    MAGMAR, PINSIR, TAUROS, MAGIKARP, GYARADOS, LAPRAS, DITTO, EEVEE, VAPOREON, JOLTEON, FLAREON, PORYGON, OMANYTE, OMASTAR,
-    KABUTO, KABUTOPS, AERODACTYL, SNORLAX, ARTICUNO, ZAPDOS, MOLTRES, DRATINI, DRAGONAIR, DRAGONITE, MEWTWO, MEW }
+     enum Species { 
+         BULBASAUR, 
+         IVYSAUR, 
+         VENUSAUR, 
+         
+         CHARMANDER, 
+         CHARMELEON, 
+         CHARIZARD, 
+         
+         SQUIRTLE, 
+         WARTORTLE, 
+         BLASTOISE, 
+
+         CATERPIE,
+         METAPOD, 
+         BUTTERFREE,       
+         WEEDLE, 
+         KAKUNA, 
+         BEEDRILL, 
+
+         PIDGEY, 
+         PIDGEOTTO, 
+         PIDGEOT,       
+         RATTATA, 
+         RATICATE, 
+         SPEAROW, 
+         FEAROW, 
+         
+         EKANS,
+         ARBOK, 
+         
+         PIKACHU, 
+         RAICHU, 
+         
+         SANDSHREW, 
+         SANDSLASH, 
+         
+         NIDORAN_F, 
+         NIDORINA, 
+         NIDOQUEEN, 
+         NIDORAN_M, 
+         NIDORINO, 
+         NIDOKING, 
+         CLEFAIRY,
+         CLEFABLE, 
+         
+         VULPIX, 
+         NINETALES, 
+         
+         JIGGLYPUFF, 
+         WIGGLYTUFF, 
+         
+         ZUBAT, 
+         GOLBAT, 
+         
+         ODDISH, 
+         GLOOM, 
+         VILEPLUME, 
+         
+         PARAS, 
+         PARASECT, 
+         VENONAT,
+         VENOMOTH, 
+         
+         DIGLETT, 
+         DUGTRIO, 
+         
+         MEOWTH, 
+         PERSIAN, 
+         
+         PSYDUCK, 
+         GOLDUCK, 
+         
+         MANKEY, 
+         PRIMEAPE, 
+         
+         GROWLITHE, 
+         ARCANINE, 
+         
+         POLIWAG, 
+         POLIWHIRL,
+         POLIWRATH, 
+         
+         ABRA, 
+         KADABRA, 
+         ALAKAZAM, 
+         
+         MACHOP, 
+         MACHOKE, 
+         MACHAMP, 
+         
+         BELLSPROUT, 
+         WEEPINBELL, 
+         VICTREEBEL, 
+         
+         TENTACOOL, 
+         TENTACRUEL,
+
+         GEODUDE, 
+         GRAVELER, 
+         GOLEM, 
+         
+         PONYTA, 
+         RAPIDASH, 
+         
+         SLOWPOKE, 
+         SLOWBRO, 
+         
+         MAGNEMITE, 
+         MAGNETON, 
+         
+         FARFETCH_D, 
+         DODUO, 
+         DODRIO, 
+         
+         SEEL,
+         DEWGONG, 
+         
+         GRIMER, 
+         MUK, 
+
+         SHELLDER, 
+         CLOYSTER, 
+         
+         GASTLY, 
+         HAUNTER, 
+         GENGAR, 
+         
+         ONIX, 
+         
+         DROWZEE, 
+         HYPNO, 
+         
+         KRABBY, 
+         KINGLER, 
+         
+         VOLTORB,
+         ELECTRODE, 
+         
+         EXEGGCUTE, 
+         EXEGGUTOR, 
+         
+         CUBONE,
+         MAROWAK, 
+
+         HITMONLEE, 
+         HITMONCHAN, 
+         LICKITUNG, 
+         
+         KOFFING, 
+         WEEZING, 
+         
+         RHYHORN, 
+         RHYDON,
+
+         CHANSEY, 
+        
+         TANGELA, 
+         
+         KANGASKHAN, 
+
+         HORSEA, 
+         SEADRA, 
+         
+         GOLDEEN, 
+         SEAKING, 
+         
+         STARYU, 
+         STARMIE, 
+
+         MR_MIME, 
+         
+         SCYTHER, 
+         
+         JYNX, 
+
+         ELECTABUZZ,
+         
+         MAGMAR, 
+         
+         PINSIR, 
+         
+         TAUROS, 
+
+         MAGIKARP, 
+         GYARADOS, 
+         LAPRAS, 
+         
+         DITTO, 
+         
+         EEVEE, 
+
+         VAPOREON, 
+         
+         JOLTEON, 
+         
+         FLAREON, 
+         
+         PORYGON, 
+
+         OMANYTE, 
+         OMASTAR,
+         KABUTO, 
+         KABUTOPS, 
+         
+         AERODACTYL, 
+         SNORLAX, 
+         
+         ARTICUNO, 
+
+         ZAPDOS, 
+         
+         MOLTRES, 
+         
+         DRATINI, 
+         DRAGONAIR, 
+         DRAGONITE, 
+         MEWTWO, 
+         MEW 
+    }
     
     // All 151 species types. Numbering follows this convention:
     //0(plant), 1(fire), 2(water), 3(bug), 4(normal), 5(poison), 6(thunder), 7(earth), 8(psychic), 9(ditto), 10(eevee)
