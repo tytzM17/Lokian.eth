cfced1c98dbf7372bd4e21d3ead4ef7c450e1d5d
theHotSteppa <azi42@yahoo.com>
2023-09-16 20:08:19 +0800

    add original and handcrafted pixel styled sprites

---
+++ src/sprites-copy/index.js
@@ -0,0 +1,311 @@
+import img0 from './0.png'
+import img1 from './1.png'
+import img10 from './10.png'
+import img100 from './100.png'
+import img101 from './101.png'
+import img102 from './102.png'
+import img103 from './103.png'
+import img104 from './104.png'
+import img105 from './105.png'
+import img106 from './106.png'
+import img107 from './107.png'
+import img108 from './108.png'
+import img109 from './109.png'
+import img11 from './11.png'
+import img110 from './110.png'
+import img111 from './111.png'
+import img112 from './112.png'
+import img113 from './113.png'
+import img114 from './114.png'
+import img115 from './115.png'
+import img116 from './116.png'
+import img117 from './117.png'
+import img118 from './118.png'
+import img119 from './119.png'
+import img12 from './12.png'
+import img120 from './120.png'
+import img121 from './121.png'
+import img122 from './122.png'
+import img123 from './123.png'
+import img124 from './124.png'
+import img125 from './125.png'
+import img126 from './126.png'
+import img127 from './127.png'
+import img129 from './129.png'
+import img13 from './13.png'
+import img130 from './130.png'
+import img131 from './131.png'
+import img132 from './132.png'
+import img133 from './133.png'
+import img134 from './134.png'
+import img135 from './135.png'
+import img136 from './136.png'
+import img137 from './137.png'
+import img138 from './138.png'
+import img139 from './139.png'
+import img14 from './14.png'
+import img140 from './140.png'
+import img141 from './141.png'
+import img142 from './142.png'
+import img143 from './143.png'
+import img144 from './144.png'
+import img145 from './145.png'
+import img146 from './146.png'
+import img147 from './147.png'
+import img148 from './148.png'
+import img149 from './149.png'
+import img15 from './15.png'
+import img150 from './150.png'
+import img151 from './151.png'
+import img16 from './16.png'
+import img17 from './17.png'
+import img18 from './18.png'
+import img19 from './19.png'
+import img2 from './2.png'
+import img20 from './20.png'
+import img21 from './21.png'
+import img22 from './22.png'
+import img23 from './23.png'
+import img24 from './24.png'
+import img25 from './25.png'
+import img26 from './26.png'
+import img27 from './27.png'
+import img28 from './28.png'
+import img29 from './29.png'
+import img3 from './3.png'
+import img30 from './30.png'
+import img31 from './31.png'
+import img32 from './32.png'
+import img33 from './33.png'
+import img34 from './34.png'
+import img35 from './35.png'
+import img36 from './36.png'
+import img37 from './37.png'
+import img38 from './38.png'
+import img39 from './39.png'
+import img4 from './4.png'
+import img40 from './40.png'
+import img41 from './41.png'
+import img42 from './42.png'
+import img43 from './43.png'
+import img44 from './44.png'
+import img45 from './45.png'
+import img46 from './46.png'
+import img47 from './47.png'
+import img48 from './48.png'
+import img49 from './49.png'
+import img5 from './5.png'
+import img50 from './50.png'
+import img51 from './51.png'
+import img52 from './52.png'
+import img53 from './53.png'
+import img54 from './54.png'
+import img55 from './55.png'
+import img56 from './56.png'
+import img57 from './57.png'
+import img58 from './58.png'
+import img59 from './59.png'
+import img6 from './6.png'
+import img60 from './60.png'
+import img61 from './61.png'
+import img63 from './63.png'
+import img64 from './64.png'
+import img66 from './66.png'
+import img67 from './67.png'
+import img68 from './68.png'
+import img69 from './69.png'
+import img7 from './7.png'
+import img70 from './70.png'
+import img71 from './71.png'
+import img72 from './72.png'
+import img73 from './73.png'
+import img74 from './74.png'
+import img75 from './75.png'
+import img76 from './76.png'
+import img77 from './77.png'
+import img78 from './78.png'
+import img79 from './79.png'
+import img8 from './8.png'
+import img80 from './80.png'
+import img81 from './81.png'
+import img82 from './82.png'
+import img83 from './83.png'
+import img84 from './84.png'
+import img85 from './85.png'
+import img86 from './86.png'
+import img87 from './87.png'
+import img88 from './88.png'
+import img89 from './89.png'
+import img9 from './9.png'
+import img90 from './90.png'
+import img91 from './91.png'
+import img92 from './92.png'
+import img93 from './93.png'
+import img94 from './94.png'
+import img95 from './95.png'
+import img96 from './96.png'
+import img98 from './98.png'
+import img99 from './99.png'
+import imgfavicon16x16 from './favicon16x16.png'
+import imgfavicon32x32 from './favicon-32x32.png'
+import imgskellyrip from './skelly-rip.png'
+import imgskelly from './skelly.png'
+import imgskelly2 from './skelly2.png'
+
+const MonImages = {
+  '0': img0,
+  '1': img1,
+  '10': img10,
+  '100': img100,
+  '101': img101,
+  '102': img102,
+  '103': img103,
+  '104': img104,
+  '105': img105,
+  '106': img106,
+  '107': img107,
+  '108': img108,
+  '109': img109,
+  '11': img11,
+  '110': img110,
+  '111': img111,
+  '112': img112,
+  '113': img113,
+  '114': img114,
+  '115': img115,
+  '116': img116,
+  '117': img117,
+  '118': img118,
+  '119': img119,
+  '12': img12,
+  '120': img120,
+  '121': img121,
+  '122': img122,
+  '123': img123,
+  '124': img124,
+  '125': img125,
+  '126': img126,
+  '127': img127,
+  '129': img129,
+  '13': img13,
+  '130': img130,
+  '131': img131,
+  '132': img132,
+  '133': img133,
+  '134': img134,
+  '135': img135,
+  '136': img136,
+  '137': img137,
+  '138': img138,
+  '139': img139,
+  '14': img14,
+  '140': img140,
+  '141': img141,
+  '142': img142,
+  '143': img143,
+  '144': img144,
+  '145': img145,
+  '146': img146,
+  '147': img147,
+  '148': img148,
+  '149': img149,
+  '15': img15,
+  '150': img150,
+  '151': img151,
+  '16': img16,
+  '17': img17,
+  '18': img18,
+  '19': img19,
+  '2': img2,
+  '20': img20,
+  '21': img21,
+  '22': img22,
+  '23': img23,
+  '24': img24,
+  '25': img25,
+  '26': img26,
+  '27': img27,
+  '28': img28,
+  '29': img29,
+  '3': img3,
+  '30': img30,
+  '31': img31,
+  '32': img32,
+  '33': img33,
+  '34': img34,
+  '35': img35,
+  '36': img36,
+  '37': img37,
+  '38': img38,
+  '39': img39,
+  '4': img4,
+  '40': img40,
+  '41': img41,
+  '42': img42,
+  '43': img43,
+  '44': img44,
+  '45': img45,
+  '46': img46,
+  '47': img47,
+  '48': img48,
+  '49': img49,
+  '5': img5,
+  '50': img50,
+  '51': img51,
+  '52': img52,
+  '53': img53,
+  '54': img54,
+  '55': img55,
+  '56': img56,
+  '57': img57,
+  '58': img58,
+  '59': img59,
+  '6': img6,
+  '60': img60,
+  '61': img61,
+  '63': img63,
+  '64': img64,
+  '66': img66,
+  '67': img67,
+  '68': img68,
+  '69': img69,
+  '7': img7,
+  '70': img70,
+  '71': img71,
+  '72': img72,
+  '73': img73,
+  '74': img74,
+  '75': img75,
+  '76': img76,
+  '77': img77,
+  '78': img78,
+  '79': img79,
+  '8': img8,
+  '80': img80,
+  '81': img81,
+  '82': img82,
+  '83': img83,
+  '84': img84,
+  '85': img85,
+  '86': img86,
+  '87': img87,
+  '88': img88,
+  '89': img89,
+  '9': img9,
+  '90': img90,
+  '91': img91,
+  '92': img92,
+  '93': img93,
+  '94': img94,
+  '95': img95,
+  '96': img96,
+  '98': img98,
+  '99': img99,
+  'skellyrip': imgskellyrip,
+  'skelly': imgskelly,
+  'skelly2': imgskelly2,
+  'favicon16x16': imgfavicon16x16,
+  'favicon32x32': imgfavicon32x32,
+}
+
+export default MonImages
